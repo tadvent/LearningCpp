e471397eca7d0caac47e44a0e87526c448ccb45c
tadvent <dantvt@gmail.com>
2014-08-07 18:20:31 +0900

    add a test case for plain array in adl.cpp

--- try_features/syntax/ADL/adl.cpp
+++ try_features/syntax/ADL/adl.cpp
@@ -4,12 +4,19 @@
 
 int main(){
     std::vector<int> v{5, 2, 8, 6, 9, 4};
-    sort(v.begin(), v.end(), [](int x, int y) -> bool{
-        return x > y;
-    });
-    for(const auto &x: v){
+    sort(v.begin(), v.end());   // OK !
+    for(const auto x: v){
         std::cout << x << ' ';
     }
     std::cout << std::endl;
+
+    int a[] = {5, 3, 7, 9, 2};
+    // sort(a, a+sizeof(a)/sizeof(a[0]));   // Error !
+    for(const auto x: a){
+        std::cout << x << ' ';
+    }
+    std::cout << std::endl;
+
+    return 0;
 }
 
