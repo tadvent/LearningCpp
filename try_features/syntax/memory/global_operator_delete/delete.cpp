e6d20cb5f198f61cfc7f89b22fc238b65076a73f
tadvent <dantvt@gmail.com>
2014-08-24 17:52:38 +0900

    add a test: overload global operator delete
    and move folder STL/memory to syntax/memory.

---
+++ try_features/syntax/memory/global_operator_delete/delete.cpp
@@ -0,0 +1,51 @@
+﻿#include <cstddef>
+#include <iostream>
+#include <cstdlib>
+using namespace std;
+
+inline void* operator new(size_t sz){
+    void *p = malloc(sz);
+    cout << "Alloc " << sz << " bytes at " << p << endl;
+    return p;
+}
+
+inline void* operator new[](size_t sz){
+    void *p = malloc(sz);
+    cout << "Alloc " << sz << " bytes at " << p << endl;
+    return p;
+}
+
+/* c++11 doesn't support size_t parameter in operator delete
+void operator delete(void *p, size_t sz){
+    free(p);
+    cout << "Free " << sz << "bytes at " << p << endl;
+}
+
+void operator delete[](void *p, size_t sz){
+    free(p);
+    cout << "Free " << sz << "bytes at " << p << endl;
+}
+*/
+
+void operator delete(void *p){
+    free(p);
+    cout << "Free at " << p << endl;
+}
+
+void operator delete[](void *p){
+    free(p);
+    cout << "Free at " << p << endl;
+}
+
+struct X{
+    int a;
+    char b[10];
+};
+
+int main(){
+    X *pa = new X{};
+    X *pb = new X[5];
+    delete pa;
+    delete [] pb;
+}
+
