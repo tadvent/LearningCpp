fa1aa10d2e0b09bbaa58893701d246bc3d1db778
tadvent <dantvt@gmail.com>
2014-09-27 18:18:32 +0900

    modify the regex test, fix bug

--- try_features/STL/regex/[w].cpp
+++ try_features/STL/regex/[w].cpp
@@ -8,8 +8,8 @@ using namespace std;
 int main(){
     regex pat("[[:w:]]+");
 
-    smatch ms;
-    if(regex_search(string("abc_def_gho"), ms, pat)){
+    cmatch ms;
+    if(regex_search("abc_def_gho", ms, pat)){
         cout << ms[0];
     }
 
